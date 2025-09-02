973c57a149510e40833886e7b717d978579c01be
Pedro Ribeiro <pedribeiro21@gmail.com>
2025-09-02 19:05:44 -0300

    hyperlinks ajustados

--- front/header.js
+++ front/header.js
@@ -1,9 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
     const mobileMenuToggle = document.querySelector('.mobile-menu-toggle');
     const mobileNav = document.querySelector('.mobile-nav');
+    const window = document.defaultView;
+    var controller = false;
 
     mobileMenuToggle.addEventListener('click', function () {
+        controller = !controller;
         mobileNav.classList.toggle('active');
         mobileMenuToggle.classList.toggle('active');
     });
+
+    window.addEventListener('resize', function(){
+        if(controller){
+        mobileNav.classList.toggle('active');
+        mobileMenuToggle.classList.toggle('active');
+        controller = false;
+        }
+    });
+
 });
