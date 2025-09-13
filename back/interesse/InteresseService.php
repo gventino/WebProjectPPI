b04de68cb3691628a6d1234abee92f4aeedcb05d
gventino <g.ventinos@gmail.com>
2025-09-13 20:54:04 -0300

    feat: interesses page backend

--- back/interesse/InteresseService.php
+++ back/interesse/InteresseService.php
@@ -2,7 +2,26 @@
 
 class InteresseService
 {
-    public function __construct(
-        public InteresseDTO $interesse,
-    ) {}
+    public InteresseRepository $repository;
+
+    public function __construct()
+    {
+        $this->repository = new InteresseRepository();
+    }
+
+    public function getInteressesByAnuncioId(int $anuncioId): array
+    {
+        try {
+            $interesses = $this->repository->getInteressesByAnuncioId($anuncioId);
+            return new MessageDTO(
+                success: true,
+                obj: $interesses
+            );
+        } catch (Throwable $e) {
+            return new MessageDTO(
+                success: false,
+                message: 'Algo deu errado durante a busca dos interesses.'
+            );
+        }
+    }
 }
