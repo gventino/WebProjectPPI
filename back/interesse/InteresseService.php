e0cff5a1ad468869314961b6b67db6f30df90e4d
Pedro Ribeiro <pedribeiro21@gmail.com>
2025-09-14 14:22:36 -0300

    Merge branch 'main' into InfinityFree

--- back/interesse/InteresseService.php
+++ back/interesse/InteresseService.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once __DIR__ . '/InteresseRepository.php';
+require_once __DIR__ . '/InteresseDTO.php';
 require_once __DIR__ . '/../messages/MessageDTO.php';
 
 class InteresseService
@@ -23,7 +24,23 @@ class InteresseService
         } catch (Throwable $e) {
             return new MessageDTO(
                 success: false,
-                message: 'Algo deu errado durante a busca dos interesses. Erro: ' . $e->getMessage()
+                message: "Algo deu errado durante a busca dos interesses - {$e->getMessage()}"
+            );
+        }
+    }
+
+    public function registerInterest(InteresseDTO $interesse): MessageDTO
+    {
+        try {
+            $success = $this->repository->registerInteresse($interesse);
+            if (!$success) {
+                throw new Exception('Could not register interesse at anuncio.');
+            }
+            return new MessageDTO(success: true);
+        } catch (Throwable $e) {
+            return new MessageDTO(
+                success: false,
+                message: "Algo deu errado durante o registro de interesses - {$e->getMessage()}"
             );
         }
     }
