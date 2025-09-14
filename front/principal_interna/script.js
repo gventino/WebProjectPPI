4c71196c72a27cf4c201c6ea14e10b87f2dc4216
Pedro Ribeiro <pedribeiro21@gmail.com>
2025-09-14 16:13:32 -0300

    fix logoff

--- front/principal_interna/script.js
+++ front/principal_interna/script.js
@@ -25,3 +25,28 @@ themeToggleButton.addEventListener('click', () => {
         nameElem.textContent = '';
     }
 })();
+
+document.querySelector('#Logoff').addEventListener('click', async function() {
+    
+    try {
+        const API_BASE_URL = `${window.location.protocol}//${window.location.host}/back/anunciante/AnuncianteController.php`;
+        const response = await fetch(API_BASE_URL, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                action: 'logout',
+            })
+        });
+        
+        const result = await response.json();
+        
+        if (result.success) {
+            alert('Logout feito!');
+            window.location.href = '../principal_externa/index.html';
+        } else {
+            alert('Erro: ' + result.message);
+        }
+    } catch (error) {
+        alert('Erro ao fazer logout');
+    }
+});
